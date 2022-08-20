3a6a36d62767a8d48cf38b303e178b8ec4e02dc6
lkram1 <ikramullah@ui.ac.id>
2022-08-20 12:20:01 +0700

    refactor(about me) : display

--- pages/experiences/index.js
+++ pages/experiences/index.js
@@ -1,133 +1,148 @@
-import { FadeWrapper, TitleWrapper } from "../../components/Wrapper";
 import {
+  Box,
+  Divider,
+  Flex,
   Heading,
-  Grid,
   Text,
   VStack,
   List,
   ListItem,
   ListIcon,
-  Box,
-  HStack,
-  Circle,
-  Flex,
+  Avatar,
   Badge,
-  Select,
 } from "@chakra-ui/react";
+import { FadeWrapper, TitleWrapper } from "../../components/Wrapper";
+import { SecondaryButton } from "../../components/Button/SecondaryButton";
 import { CheckCircleIcon } from "@chakra-ui/icons";
-import Head from "next/head";
-import { PrimaryButton, SecondaryButton } from "../../components/Button/";
 
 const Experiences = () => {
   return (
-    <>
-      <Head>
-        <title>Experience</title>
-      </Head>
-      <FadeWrapper>
-        {" "}
-        <TitleWrapper>
-          <Heading fontSize={{ base: "2xl", lg: "5xl" }}>Experience</Heading>
-          <Text fontSize={{ lg: "1.5rem", base: "16px" }}>
+    <FadeWrapper>
+      <VStack w="100%" gap="3em">
+        <TitleWrapper textAlign="center">
+          <Heading fontSize={{ base: "2xl", lg: "5xl" }}>Experiences</Heading>
+          <Text fontSize={{ lg: "1.2rem", base: "16px" }}>
             That i have taken part of.
           </Text>
         </TitleWrapper>
-        <Grid
-          mt="2em"
-          gridTemplateColumns={{ lg: "1fr 2fr", base: "1fr" }}
-          gap={{ lg: "5em", base: "1em" }}
-          width="100%"
-          pb="3em"
+        <SecondaryButton alignSelf="end">2022</SecondaryButton>
+      </VStack>
+      <VStack
+        mt="1em"
+        w="100%"
+        p={{ lg: "0 6em" }}
+        gap="0.5em"
+        alignItems={"start"}
+      >
+        <Flex ml={{ lg: "-3em", base: "-2em" }} alignItems={"center"} w="100%">
+          <Text minW="100px">2022 January</Text>
+          <Divider />
+        </Flex>
+        <VStack
+          alignItems={"start"}
+          marginLeft="1em"
+          w="100%"
+          borderLeft={"1px solid white"}
         >
-          <Select
-            backgroundColor={"primary.900"}
-            borderColor={"secondary.900"}
-            _focus={{ borderColor: "secondary.900" }}
-            _hover={{ borderColor: "secondary.900" }}
-            display={{ base: "block", lg: "None" }}
-          >
-            <option
-              style={{ backgroundColor: "#100720" }}
-              value={"Lorem ipsum"}
-            >
-              Lorem Ipsum
-            </option>
-            <option
-              value={"Lorem ipsum"}
-              style={{ backgroundColor: "#100720" }}
-            >
-              Lorem Ipsum
-            </option>
-            <option
-              style={{ backgroundColor: "#100720" }}
-              value={"Lorem ipsum"}
-            >
-              Lorem Ipsum
-            </option>
-          </Select>
-          <VStack
-            display={{ base: "None", lg: "flex" }}
-            alignItems={"stretch"}
-            borderRight={{ lg: "2px solid white" }}
-            borderBottom={{ lg: "none", base: "2px solid white" }}
-            pr={{ lg: "3em" }}
-            pb={{ base: "2em" }}
-          >
-            
-            <SecondaryButton justifyContent="start">
-              1. Lorem Ipsum{" "}
-            </SecondaryButton>
-            <SecondaryButton justifyContent="start">
-              2. Lorem Ipsum{" "}
-            </SecondaryButton>
-            <SecondaryButton justifyContent="start">
-              3. Lorem Ipsum{" "}
-            </SecondaryButton>
-          </VStack>
-          <VStack gap={"0.5em"} mt={{ base: "1em" }} alignItems={"start"}>
-            <Flex
-              w={"100%"}
-              alignItems="center"
-              justifyContent={"space-between"}
-            >
-              <Heading fontSize={{ base: "17px", lg: "3xl" }}>
-                Software Engineer
-              </Heading>
-              <PrimaryButton>Detail</PrimaryButton>
-            </Flex>
-            <Text fontSize={"xl"}>@Company</Text>
-            <Text as={"i"}>January 2020 - February 2021 </Text>
+          <Flex alignItems={"center"} position="relative" w="100%">
+            <Box mt="1em" ml="3em" w="100%">
+              <Flex
+                alignItems={{ lg: "center", base: "start" }}
+                flexDir={{ lg: "row", base: "column" }}
+                justifyContent="space-between"
+              >
+                <Avatar
+                  position="absolute"
+                  bg="white"
+                  left="-1.3em"
+                  src={"/itf.svg"}
+                />
 
-            <Text maxW={"80ch"}>
-              Short description of what the company is about. Then, write down
-              what you are doing during those period of time, be it sleeping,
-              watching, or even doing the real work.
-            </Text>
-            <Box>
-              My contribution :
-              <List mt="1em" spacing={3} pl="1em">
+                <Heading fontSize={{ base: "17px", lg: "2xl" }}>
+                  {"Junior Developer @ITF"}
+                </Heading>
+                <Text>6 months</Text>
+              </Flex>
+              <Text mt="0.3em">
+                one of FUKI's bureau. Act as mantainer for FUKI technology,
+                mainly websites.
+              </Text>
+
+              <List mt="0.3em" ml="1em">
                 <ListItem>
                   <ListIcon as={CheckCircleIcon} color="green.500" />
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit
+                  Create 404 page on mentoring web.
                 </ListItem>
                 <ListItem>
                   <ListIcon as={CheckCircleIcon} color="green.500" />
-                  Assumenda, quia temporibus eveniet a libero incidunt suscipit
+                  Create landing page on web FUKI.
+                </ListItem>
+                <ListItem>
+                  <ListIcon as={CheckCircleIcon} color="green.500" />
+                  Create rest api on web mentoring using django restframework.
                 </ListItem>
               </List>
             </Box>
-            <Box>
-              Honorable mention teammates :
-              <HStack>
-                <Circle w="50px" h="50px" bgColor={"gray"} />{" "}
-                <Circle w="50px" h="50px" bgColor={"gray"} />{" "}
-                <Circle w="50px" h="50px" bgColor={"gray"} />
-              </HStack>
+          </Flex>
+        </VStack>
+      </VStack>
+      <VStack
+        mt="1em"
+        w="100%"
+        p={{ lg: "0 6em" }}
+        gap="0.5em"
+        alignItems={"start"}
+      >
+        <Flex ml={{ lg: "-3em", base: "-2em" }} alignItems={"center"} w="100%">
+          <Text minW="100px" display={"inline-block"}>
+            2022 January
+          </Text>
+          <Divider />
+        </Flex>
+        <VStack alignItems={"start"} w="100%" borderLeft={"1px solid white"}>
+          <Flex alignItems={"center"} position="relative" w="100%">
+            <Box mt="1em" ml="3em" w="100%">
+              <Flex
+                alignItems={{ lg: "center", base: "start" }}
+                flexDir={{ lg: "row", base: "column" }}
+                justifyContent="space-between"
+              >
+                <Avatar
+                  position="absolute"
+                  bg="white"
+                  left="-1.3em"
+                  src={"/itf.svg"}
+                />
+
+                <Heading fontSize={{ base: "17px", lg: "2xl" }}>
+                  {"Junior Developer @ITF"}
+                </Heading>
+                <Text>6 months</Text>
+              </Flex>
+              <Text mt="0.3em">
+                one of FUKI's bureau. Act as mantainer for FUKI technology,
+                mainly websites.
+              </Text>
+
+              <List mt="0.3em" ml="1em">
+                <ListItem>
+                  <ListIcon as={CheckCircleIcon} color="green.500" />
+                  Create 404 page on mentoring web.
+                </ListItem>
+                <ListItem>
+                  <ListIcon as={CheckCircleIcon} color="green.500" />
+                  Create landing page on web FUKI.
+                </ListItem>
+                <ListItem>
+                  <ListIcon as={CheckCircleIcon} color="green.500" />
+                  Create rest api on web mentoring using django restframework.
+                </ListItem>
+              </List>
             </Box>
-          </VStack>
-        </Grid>
-      </FadeWrapper>
-    </>
+          </Flex>
+        </VStack>
+      </VStack>
+    </FadeWrapper>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,11 @@
-import { Flex, Heading, StackDivider, Text, VStack } from "@chakra-ui/react";
+import {
+  Center,
+  Flex,
+  Heading,
+  StackDivider,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { useState } from "react";
@@ -29,9 +36,14 @@ export default function Home() {
   };
 
   return (
-    <Flex alignItems={!aboutMe && "center"} height="100%">
+    <Flex
+      alignItems={!aboutMe && "center"}
+      h={!aboutMe && "100%"}
+      pb="1em"
+      w="100%"
+    >
       <Head>
-        <title>Home</title>
+        <title>Hello There!</title>
       </Head>
       {!aboutMe && (
         <FadeWrapper userSelect={"none"}>
@@ -65,77 +77,85 @@ export default function Home() {
         </FadeWrapper>
       )}
       {aboutMe && (
-        <FadeWrapper>
+        <FadeWrapper
+          w="100%"
+          display="flex"
+          flexDir="column"
+          alignItems="center"
+        >
           <TitleWrapper>
             <Heading fontSize={{ base: "3xl", lg: "5xl" }}>About me</Heading>
           </TitleWrapper>
-          <VStack
-            mt="1em"
-            gap="0.5em"
-            alignItems={"start"}
-            divider={<StackDivider />}
-          >
-            <>
-              <Text fontWeight={"500"} fontSize={"2xl"}>
-                Background 🍀
-              </Text>
-              <Text maxW={"90ch"} lineHeight={"1.7"}>
-                Hello there! {"I'm"} Ikramullah, you can call me ikram. I was born
-                in 4th of July 2002, which is the independence day of America.
-                Though, i was born in Indonesia, Banda Aceh ,the most western
-                part of Indonesia. I grew up there until 20 years old and moved
-                to Depok for my education.
-              </Text>
-            </>
-            <>
-              <Text fontWeight={"500"} fontSize={"2xl"}>
-                Education 🏫
-              </Text>
+          <Flex alignItems={"center"}>
+            <VStack
+              mt="1em"
+              gap="0.5em"
+              alignItems={"start"}
+              divider={<StackDivider />}
+            >
+              <>
+                <Text fontWeight={"500"} fontSize={"2xl"}>
+                  Background 🍀
+                </Text>
+                <Text maxW={"90ch"} lineHeight={"1.7"}>
+                  Hello there! {"I'm"} Ikramullah, you can call me ikram. I was
+                  born in 4th of July 2002, which is the independence day of
+                  America. Though, i was born in Indonesia, Banda Aceh ,the most
+                  western part of Indonesia. I grew up there until 20 years old
+                  and moved to Depok for my education.
+                </Text>
+              </>
+              <>
+                <Text fontWeight={"500"} fontSize={"2xl"}>
+                  Education 🏫
+                </Text>
 
-              <Text maxW={"90ch"} lineHeight={"1.7"}>
-                I am currently studying in Universitas Indonesia, taking
-                Computer Science degree. I actually have no programming
-                experience, i chose Computer science just because. Turns out, i
-                really enjoyed it and now {"i'm"} at my 2nd year.
-              </Text>
-            </>
-            <>
-              <Text fontWeight={"500"} fontSize={"2xl"}>
-                My Coding Experience 💻
-              </Text>
+                <Text maxW={"90ch"} lineHeight={"1.7"}>
+                  I am currently studying in Universitas Indonesia, taking
+                  Computer Science degree. I actually have no programming
+                  experience, i chose Computer science just because. Turns out,
+                  i really enjoyed it and now {"i'm"} at my 2nd year.
+                </Text>
+              </>
+              <>
+                <Text fontWeight={"500"} fontSize={"2xl"}>
+                  My Coding Experience 💻
+                </Text>
 
-              <Text maxW={"90ch"} lineHeight={"1.7"}>
-                The first time i wrote a program when i entered college. My very
-                first language is python and i was so confused on what to write,
-                so i kinda hate it at first. But then, one day, my friend had an
-                assignment which is like a puzzle and since i liked the problem
-                i tried to solve it.
-              </Text>
+                <Text maxW={"90ch"} lineHeight={"1.7"}>
+                  The first time i wrote a program when i entered college. My
+                  very first language is python and i was so confused on what to
+                  write, so i kinda hate it at first. But then, one day, my
+                  friend had an assignment which is like a puzzle and since i
+                  liked the problem i tried to solve it.
+                </Text>
 
-              <Text maxW={"90ch"} lineHeight={"1.7"}>
-                The problem was N-Queen problem, where you tried to put 8 queen
-                on a chess board and make sure that each queen does not attack
-                each other. I tried to create the program using python and i was
-                so invested on it that i spent 2 days trying to solve it. I
-                solved it and this is where my interest of programming started (
-                thank you N-Queen ).
-              </Text>
+                <Text maxW={"90ch"} lineHeight={"1.7"}>
+                  The problem was N-Queen problem, where you tried to put 8
+                  queen on a chess board and make sure that each queen does not
+                  attack each other. I tried to create the program using python
+                  and i was so invested on it that i spent 2 days trying to
+                  solve it. I solved it and this is where my interest of
+                  programming started.
+                </Text>
 
-              <Text maxW={"90ch"} lineHeight={"1.7"}>
-                Later on, i found out that there are a lot of things that you
-                can do with programming, such as web development, AI, Software
-                Engineer, and many more! Right now, {"i'm"} trying to be a fullstack
-                software engineer, so there are still a lot of things i should learn 📚.
-              </Text>
-            </>
-          </VStack>
-          <SecondaryButton
-            callback={() => {
-              setAboutMe(false);
-            }}
-          >
-            Go back
-          </SecondaryButton>
+                <Text maxW={"90ch"} lineHeight={"1.7"}>
+                  Later on, i found out that there are a lot of things that you
+                  can do with programming, such as web development, AI, Software
+                  Engineer, and many more! Right now, {"i'm"} trying to be a
+                  fullstack software engineer, so there are still a lot of
+                  things i should learn 📚.
+                </Text>
+              </>
+              <SecondaryButton
+                callback={() => {
+                  setAboutMe(false);
+                }}
+              >
+                Go back
+              </SecondaryButton>
+            </VStack>
+          </Flex>
         </FadeWrapper>
       )}
     </Flex>
